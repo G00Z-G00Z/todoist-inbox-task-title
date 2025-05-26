b896205ff1b1beacb034f2543ba3815320ddc6f9
Gomez Eduardo MNR TDPRB <eduardo.gomez2@zf.com>
2025-05-26 16:07:55 -0600

    Boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # todoist-inbox-task-title
+
 Small script that retrieves the tasks titles for todoist. This can be used later to integrate with another app
+
+## Development
+
+It uses `uv` as a python packet manager
---
+++ todoist_inbox_retrieve.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "httpx",
+#     "pydantic",
+# ]
+# ///
+
+
+def main() -> None:
+    print("Hello from todoist_inbox_retrieve.py!")
+
+
+if __name__ == "__main__":
+    main()
